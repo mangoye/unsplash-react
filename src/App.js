02034a7a1948fe6bf8ebe0f3ebacba307a68ecd2
Sidney Mangoye <mangoyesidney@gmail.com>
2020-11-29 01:50:34 +0100

    add css and app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+        <h1 className="title">React Photo Search</h1>
+      </div>
     </div>
   );
 }
